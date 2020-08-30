32598a1f11c33b779bbdcbf070b4701ed0813c0b
김재구 <rlaworn1993@gmail.com>
2020-08-30 16:58:04 +0900

    implemeted UserService

---
+++ gameTodoeyBackend/src/main/java/com/gameTodoeyBackend/service/UserService.java
@@ -0,0 +1,17 @@
+package com.gameTodoeyBackend.service;
+
+import java.util.List;
+
+import com.gameTodoeyBackend.entity.User;
+
+public interface UserService {
+
+	public List<User> getCustomers();
+
+	public void saveUser(User theCustomer);
+
+	public User getUser(int theId);
+
+	public void deleteUser(int theId);
+	
+}
