6b83bd527a25eb22218de97e85cb13f56c2d671d
김재구 <rlaworn1993@gmail.com>
2020-08-30 17:27:41 +0900

    added create-user.sql

---
+++ create-users.sql
@@ -0,0 +1,22 @@
+LOCK TABLES `user` WRITE;
+
+INSERT INTO `user` VALUES 
+	(1,'kildong','hong','hong@gmail.com'),
+	(2,'John','Doe','john@luv2code.com'),
+	(3,'Ajay','Rao','ajay@luv2code.com'),
+	(4,'Mary','Public','mary@luv2code.com'),
+	(5,'Maxwell','Dixon','max@luv2code.com');
+
+/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-24 21:50:59
