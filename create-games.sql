f2cfbb60401f7f000ec2f380be193bf308321db3
김재구 <rlaworn1993@gmail.com>
2020-08-31 17:32:15 +0900

    inserted data of games

---
+++ create-games.sql
@@ -0,0 +1,10 @@
+LOCK TABLES `game` WRITE;
+
+INSERT INTO `game` VALUES 
+	(1,'Last Of Us Part2',0),
+	(2,'Halo Master Chief Collection',0),
+    (3,'Zelda Breath Of Wild',0),
+    (4,'God Of War 4',0),
+    (5,'Ghost Of Thushima',0);
+    
+UNLOCK TABLES;
