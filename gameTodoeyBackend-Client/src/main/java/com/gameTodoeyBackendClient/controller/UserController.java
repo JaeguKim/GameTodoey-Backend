02e71256cc25f41c23504f3a6cbb5358ae92df3c
김재구 <rlaworn1993@gmail.com>
2020-08-31 14:18:41 +0900

    added jsp page

--- gameTodoeyBackend-Client/src/main/java/com/gameTodoeyBackendClient/controller/UserController.java
+++ gameTodoeyBackend-Client/src/main/java/com/gameTodoeyBackendClient/controller/UserController.java
@@ -1,11 +1,70 @@
 package com.gameTodoeyBackendClient.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.gameTodoeyBackendClient.model.User;
+import com.gameTodoeyBackendClient.service.UserService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
+	@Autowired 
+	UserService userService;
 	
+	@GetMapping("/list")
+	public String listUsers(Model theModel) {
+		
+		List<User> theUsers = userService.getUsers();
+		
+		theModel.addAttribute("users",theUsers);
+		
+		return "list-users";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		User theUser = new User();
+		
+		theModel.addAttribute("user",theUser);
+		
+		return "user-form";
+	}
+	
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute("user") User theUser) {
+		
+		userService.saveUser(theUser);
+		
+		return "redirect:/user/list";
+	}
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("userId") int theId, Model theModel) {
+		
+		User theUser = userService.getUser(theId);	
+		
+		theModel.addAttribute("user", theUser);
+				
+		return "user-form";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteUser(@RequestParam("userId") int theId) {
+		
+		// delete the customer
+		userService.deleteUser(theId);
+		
+		return "redirect:/user/list";
+	}
 }
