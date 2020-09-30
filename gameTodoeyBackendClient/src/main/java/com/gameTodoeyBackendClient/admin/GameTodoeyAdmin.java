2cd422e6010f86dcd7eead9bd496b62def452b1d
김재구 <rlaworn1993@gmail.com>
2020-09-30 10:37:12 +0900

    09.29

---
+++ gameTodoeyBackendClient/src/main/java/com/gameTodoeyBackendClient/admin/GameTodoeyAdmin.java
@@ -0,0 +1,37 @@
+package com.gameTodoeyBackendClient.admin;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class GameTodoeyAdmin {
+
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")	
+	private String userName;
+	
+	@NotNull(message="is required")
+	@Size(min=1, message="is required")
+	private String password;
+	
+	public GameTodoeyAdmin() {
+		
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
--- gameTodoeyBackendClient/src/main/java/com/gameTodoeyBackendClient/config/AppConfig.java
+++ gameTodoeyBackendClient/src/main/java/com/gameTodoeyBackendClient/config/AppConfig.java
@@ -1,9 +1,17 @@
 package com.gameTodoeyBackendClient.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,11 +19,19 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
 @ComponentScan("com.gameTodoeyBackendClient")
-@PropertySource({ "classpath:application.properties" })
+@PropertySource({ "classpath:application.properties","classpath:security-persistence-mysql.properties" })
 public class AppConfig implements WebMvcConfigurer {
+
+	@Autowired
+	private Environment env;
+
+	private Logger logger = Logger.getLogger(getClass().getName());
 
 	// define a bean for ViewResolver
 	@Bean
@@ -43,6 +59,67 @@ public class AppConfig implements WebMvcConfigurer {
 		registry
 		.addResourceHandler("/resources/**")
 		.addResourceLocations("/resources/"); 
+	}	
+
+	// define a bean for our security datasource
+	
+	@Bean
+	public DataSource securityDataSource() {
+		
+		// create connection pool
+		ComboPooledDataSource securityDataSource
+									= new ComboPooledDataSource();
+				
+		// set the jdbc driver class
+		
+		try {
+			securityDataSource.setDriverClass(env.getProperty("security.jdbc.driver"));
+		} catch (PropertyVetoException exc) {
+			throw new RuntimeException(exc);
+		}
+		
+		// log the connection props
+		// for sanity's sake, log this info
+		// just to make sure we are REALLY reading data from properties file
+		
+		logger.info(">>> security.jdbc.url=" + env.getProperty("security.jdbc.url"));
+		logger.info(">>> security.jdbc.user=" + env.getProperty("security.jdbc.user"));
+		
+		
+		// set database connection props
+		
+		securityDataSource.setJdbcUrl(env.getProperty("security.jdbc.url"));
+		securityDataSource.setUser(env.getProperty("security.jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("security.jdbc.password"));
+		
+		// set connection pool props
+		
+		securityDataSource.setInitialPoolSize(
+				getIntProperty("security.connection.pool.initialPoolSize"));
+
+		securityDataSource.setMinPoolSize(
+				getIntProperty("security.connection.pool.minPoolSize"));
+
+		securityDataSource.setMaxPoolSize(
+				getIntProperty("security.connection.pool.maxPoolSize"));
+
+		securityDataSource.setMaxIdleTime(
+				getIntProperty("security.connection.pool.maxIdleTime"));
+		
+		return securityDataSource;
+	}
+	
+	// need a helper method 
+	// read environment property and convert to int
+	
+	private int getIntProperty(String propName) {
+		
+		String propVal = env.getProperty(propName);
+		
+		// now convert to int
+		int intPropVal = Integer.parseInt(propVal);
+		
+		return intPropVal;
 	}	
 
 }
