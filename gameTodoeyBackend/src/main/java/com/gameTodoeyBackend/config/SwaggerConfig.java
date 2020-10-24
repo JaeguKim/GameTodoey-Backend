fc603d39edfd2cbfb07a2d85d17c62d9da158dd1
김재구 <rlaworn1993@gmail.com>
2020-10-24 21:04:40 +0900

    [DOC] adding api doc

---
+++ gameTodoeyBackend/src/main/java/com/gameTodoeyBackend/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.gameTodoeyBackend.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport{
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.useDefaultResponseMessages(false)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.gameTodoeyBackend"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(metaData());
+
+	}
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder()
+				.title("GameTodoey Admin REST API")
+				//.description("GameTodoey Admin : api for ")
+				.version("0.0.0")
+				.contact(new Contact("Jaegoo Kim", "https://github.com/JaeguKim/GameTodoey-Backend", "kimwithglasses@naver.com"))
+				.build();
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("swagger-ui.html")
+	      .addResourceLocations("classpath:/META-INF/resources/");
+	 
+	    registry.addResourceHandler("/webjars/**")
+	      .addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
+}
+
--- gameTodoeyBackend/src/main/webapp/index.jsp
+++ gameTodoeyBackend/src/main/webapp/index.jsp
@@ -3,7 +3,7 @@
 <h3>Spring REST Demo</h3>
 <hr>
 
-<a href="${pageContext.request.contextPath}/test/hello">Hello</a>
+<a href="${pageContext.request.contextPath}/swagger-ui.html#/">Hello</a>
 
 </body>
 </html>
